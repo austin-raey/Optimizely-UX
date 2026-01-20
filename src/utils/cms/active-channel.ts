ad03dccd524291bcfc5cd2463d8c60e6b2e7269d
Austin Meadows <slowpoke@slowp.ke>
2026-01-20 14:53:25 -0500

    add potential future example

--- src/utils/cms/active-channel.ts
+++ src/utils/cms/active-channel.ts
@@ -8,3 +8,27 @@ export const ACTIVE_CHANNEL = (
 		nyee: "https://optimizely-ux-five.vercel.app",
 	} satisfies Record<typeof env.OPTIMIZELY_CHANNEL, string>
 )[env.OPTIMIZELY_CHANNEL];
+
+/*
+// FUTURE:
+export const ACTIVE_CHANNEL = (
+	{
+		dev: {
+			icahn: "https://example-icahn-dev.vercel.app",
+			mshealth: "https://example-mshealth-dev.vercel.app",
+			nyee: "https://example-nyee-dev.vercel.app",
+		},
+		stage: {
+			icahn: "https://example-icahn-stage.vercel.app",
+			mshealth: "https://example-mshealth-stage.vercel.app",
+			nyee: "https://example-nyee-stage.vercel.app",
+		},
+		// eslint-disable-next-line perfectionist/sort-objects
+		prod: {
+			icahn: "https://example-prod-icahn.vercel.app",
+			mshealth: "https://example-prod-mshealth.vercel.app",
+			nyee: "https://example-prod-nyee.vercel.app",
+		},
+	} satisfies Record<"dev" | "prod" | "stage", Record<typeof env.OPTIMIZELY_CHANNEL, string>>
+)["prod"][env.OPTIMIZELY_CHANNEL];
+*/
