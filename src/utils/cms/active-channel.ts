d8c6030f226626998458c56dfc515b892d64d326
Austin Meadows <slowpoke@slowp.ke>
2026-01-20 14:49:58 -0500

    fix order

--- src/utils/cms/active-channel.ts
+++ src/utils/cms/active-channel.ts
@@ -3,8 +3,8 @@ import { env } from "@/env";
 // TODO: Will need to include seperate channels per environment as well (default dev)
 export const ACTIVE_CHANNEL = (
 	{
-		icahn: "https://optimizely-ux-five.vercel.app",
+		icahn: "https://optimizely-ux.vercel.app",
 		mshealth: "https://20251021moseyhealthpoc.vercel.app",
-		nyee: "https://optimizely-ux.vercel.app",
+		nyee: "https://optimizely-ux-five.vercel.app",
 	} satisfies Record<typeof env.OPTIMIZELY_CHANNEL, string>
 )[env.OPTIMIZELY_CHANNEL];
